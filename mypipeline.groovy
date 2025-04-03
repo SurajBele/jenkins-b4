7a46fbdafca3eb42a2e76e6d678fba1d6c0490e7
suraj <surajbele51@gmail.com>
2025-04-03 12:57:58 +0530

    --- jenkins updated pipeline ---

--- mypipeline.groovy
+++ mypipeline.groovy
@@ -20,6 +20,12 @@ pipeline {
         }
         stage('testing') { 
             steps {
+                script {
+                    // Switch to Java 17 for SonarQube
+                    env.JAVA_HOME = tool 'openjdk17'
+                    env.PATH = "${env.JAVA_HOME}/bin:${env.PATH}"
+                }
+                sh 'java -version'  // Verify Java 17 is active
                 
                 withSonarQubeEnv(installationName: 'sonar-server', credentialsId: 'sonar-token') {
                  sh 'mvn clean package sonar:sonar -Dsonar.projectKey=myproject'
